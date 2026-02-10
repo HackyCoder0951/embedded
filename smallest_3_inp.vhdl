b0832145b3131d7b7a3f328a38895f08c6236b7f
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2026-02-11 00:38:43 +0530

    Refactor smallest_3_inp VHDL entity and architecture

--- smallest_3_inp.vhdl
+++ smallest_3_inp.vhdl
@@ -4,9 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity smallest_3_inp is
     Port (
-        A   : in  signed(3 downto 0);
-        B   : in  signed(3 downto 0);
-        C   : in  signed(3 downto 0);
+        A,B,C   : in  signed(3 downto 0);
         MIN : out signed(3 downto 0)
     );
 end smallest_3_inp;
@@ -24,4 +22,3 @@ begin
         end if;
     end process;
 end behavioral;
-
